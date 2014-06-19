ba6386d024f6150e57748ac27880be1ca71d74a9
ivan <ivan@master.(none)>
2014-06-19 04:18:37 +0000

    refer to ansible in /usr/local/src/ansible

--- setup_env.sh
+++ setup_env.sh
@@ -1,8 +1,7 @@
-alias ansible='/usr/local/src/ansible-1.6.1/bin/ansible'
 alias a='ansible'
 alias ap='ansible-playbook'
 export RAX_REGION=DFW
 export RAX_EXCLUDE_HOST=master
 export ANSIBLE_CONFIG=/home/ansible/ansible.cfg
-source /usr/local/src/ansible-1.6.1/hacking/env-setup > /dev/null
+source /usr/local/src/ansible/hacking/env-setup > /dev/null
 
