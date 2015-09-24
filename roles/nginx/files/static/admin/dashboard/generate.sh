d89596691f81705f62d740d07bac462198c5b869
Alex Younts <alex@tinkergeek.com>
2015-09-24 09:58:47 -0700

    Remove sc10009 from nagios. Also the prototype dashboard script with associated files.

---
+++ roles/nginx/files/static/admin/dashboard/generate.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# generate index.html
+cat header.html > index.new
+echo '<div class="col-xs-6">' >> index.new
+for server in aldebaran betelgeuse
+do
+	./get_appserver.pl $server >> index.new
+done
+echo "</div>" >> index.new
+echo '<div class="col-xs-6">' >> index.new
+for server in capella deneb
+do
+	./get_appserver.pl $server >> index.new
+done
+echo "</div>" >> index.new
+cat footer.html >> index.new
+mv index.new index.html
+
+# generate capella.html
+cat header.html > capella.new
+./get_appserver.pl capella >> capella.new
+./get_sc.pl sc10022 30022 >> capella.new
+cat footer.html >> capella.new
+mv capella.new capella.html
+
+# generate betelgeuse.html
+cat header.html > betelgeuse.new
+./get_appserver.pl betelgeuse >> betelgeuse.new
+./get_sc.pl sc10004 30004 >> betelgeuse.new
+cat footer.html >> betelgeuse.new
+mv betelgeuse.new betelgeuse.html
+
+# generate deneb.html
+cat header.html > deneb.new
+./get_appserver.pl deneb >> deneb.new
+cat footer.html >> deneb.new
+mv deneb.new deneb.html
+
+# generate aldebaran.html
+cat header.html > aldebaran.new
+./get_appserver.pl aldebaran >> aldebaran.new
+cat footer.html >> aldebaran.new
+mv aldebaran.new aldebaran.html
+
+exit 0
