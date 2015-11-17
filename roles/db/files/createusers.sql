26c09e86b1b79899fbac8dd0d9c5c7b900b9492f
Alex Younts <alex.younts@codeshelf.com>
2015-11-17 18:33:39 +0000

    started work on database role

--- roles/db/files/createusers.sql
+++
@@ -1,5 +0,0 @@
-create database database;
-create user codeshelf with password 'medrogarkinagife';
-grant all privileges on database database to codeshelf;
-CREATE USER replicator REPLICATION LOGIN ENCRYPTED PASSWORD 'hrajukipnoldwary';
-
